965187adc77bb9b87cdfafbc404edfd53cfbdb79
Riham274 <riham.ararawi274@gmail.com>
2025-10-03 21:26:55 +0300

    Fix button styles to display numbers correctly

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = {
 function Button({ onClick, color, name }: ButtonProps) {
   return (
     <button
-      className={`px-4 py-2 ${color} text-white rounded-xl transition font-semibold`}
+      className={`px-4 py-2 ${color} text-white rounded-xl transition font-semibold cursor-pointer`}
       onClick={onClick}
     >
       {name}
